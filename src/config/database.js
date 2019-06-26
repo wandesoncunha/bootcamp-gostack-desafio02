5cd6bcaaaf5d991eb8cfc0b2a15befca53e5262a
Wandeson Cunha <wandesoncunha@rits.com.br>
2019-06-26 13:46:57 -0300

    add User model

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'w@nd350n',
+  database: 'gostack',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
